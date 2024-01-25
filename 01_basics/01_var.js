ab102748e66adbfc185ae4994c4a49f1fdc36bb0
shriiiiiii <156116371+shriiiiiii@users.noreply.github.com>
2024-01-25 17:43:08 +0000

    config js

---
+++ 01_basics/01_var.js
@@ -0,0 +1,15 @@
+const accountId=2224;
+let accountState="Mumbai";
+var accountName="shrikanth";
+var accountEmail="huehueh@gmail.com";
+let salary;
+
+
+// var let const are keywords which are used to store data in the memory,accname .....are variable name where it stores the data 
+
+// use only let const 
+// dont use var ,because it cause problem in block scope and function scope{} for example a dev declears the variable name in the scope and outside the function too then, the javascript gets confused
+
+// instead of console log one by one we can use table 
+console.table([accountId,accountEmail,accountName,accountState,salary]);
+// a variable without any value is known as undefineds
