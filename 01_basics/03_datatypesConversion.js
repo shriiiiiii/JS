33d7cb3b8f7381bcb3cf1dd0dc1a043658458233
shriiiiiii <156116371+shriiiiiii@users.noreply.github.com>
2024-01-25 18:43:11 +0000

    datatypes conversion

---
+++ 01_basics/03_datatypesConversion.js
@@ -0,0 +1,18 @@
+// let id="24";
+//here the number is given string,sometime when we create form in website t will return data in the form of string to overcome this issue we can use conversion
+// let idNum=Number(id);
+// console.log(typeof idNum);//number
+// console.log(idNum);//24
+//incase there are some alphabets in the number then what
+
+
+/*let id="24shr";
+console.log(typeof id);
+console.log(id);
+let idNum=Number(id);
+console.log(idNum);//nan not a number 
+console.log(typeof idNum);//number
+console.log(typeof NaN);//number*/
+
+//till now we have discuss about conversion of string to number what abt number to string
+let id=24;
