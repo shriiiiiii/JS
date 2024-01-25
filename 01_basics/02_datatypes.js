d204dac7fa7f83ecb2c9cca772a48df4ee8bb7a5
shriiiiiii <156116371+shriiiiiii@users.noreply.github.com>
2024-01-25 18:14:57 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict";//treeat all js code as a newer version
+
+// alert("hitesh");it will not work in node,it will only work on browsers
+
+/* types of datatype   
+    string => ""
+    number => 23
+    boolean => true/false
+    bigint => in js number datatype can hold up to certain value only,
+    bigint can save value larger than number ,we wont use it that much..
+    undefined => a varaible with no values
+    null =>is datatype ,but for now consider it as standalone value
+    symbol => mainly used in react js 
+ 
+
+to check type of data type use this syntax 
+*/
+let val="shrika";
+console.log(typeof val);//string
+// but if you check typeof null 
+let add=null;
+console.log(typeof add);//it will return object
